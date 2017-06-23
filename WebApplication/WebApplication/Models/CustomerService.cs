e19fbaa483119cfaf28d1b99391df724cd2d98c9
育軒 詹 <qazazqzw@gmail.com>
2017-06-23 16:04:26 +0800

    final

---
+++ WebApplication/WebApplication/Models/CustomerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using static WebApplication.Controllers.OrderController;
+namespace WebApplication.Models
+{
+    public class CustomerService
+    {
+        Model1 db = new Model1();
+        public Models.Customers GetOrderById(int CustomerID)
+        {
+            return db.Customers.Where(x => x.CustomerID == CustomerID).FirstOrDefault();
+        }
+        public List<Models.Customers> GetOrderByCondition(SearchCondition sc)
+        {
+
+            return db.Customers
+                    .Where(x => sc.CustomerID == null || x.CustomerID == sc.CustomerID)
+                    .ToList();
+        }
+    }
+    
+}
---
+++ WebApplication/WebApplication/fonts/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET: Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public class SearchCondition
+        {
+            public int? CustomerID { get; set; }
+        }
+        public JsonResult Search(SearchCondition sc)
+        {
+            Models.CustomerService CustomerService = new Models.CustomerService();
+            List<Models.Customers> customers = CustomerService.GetOrderByCondition(sc);
+            List<Object> results = new List<Object>();
+            for (int i = 0; i < customers.Count; i++)
+            {
+                Models.Customers order = customers[i];
+                results.Add(new
+                {
+                    CustomerID = order.CustomerID,
+                });
+            }
+            return Json(results, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
